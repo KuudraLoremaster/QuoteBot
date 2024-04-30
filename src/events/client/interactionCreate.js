a639a00d1d1e5a823364855f558e356ab0376173
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-04-30 18:48:29 +0300

    im stupid and i hate myself

---
+++ src/events/client/interactionCreate.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (interaction.isChatInputCommand()) {
+            const {commands} = client;
+            const {commandName} = interaction;
+            const command = commands.get(commandName)
+            if (!command) return;
+            if (interaction == 'quote-daily') {
+                nextDaily = new Date(new Date().getTime() + (24 * 60 * 60 * 1000));
+                cooldown = new Date(nextDaily - Date.now())
+            }
+            try {
+                await command.execute(interaction, client);
+                console.log(`${interaction.user.tag} ran command ${interaction}`)
+                // console.log(typeof users)
+            } catch (error) {
+                console.error(error)
+                await interaction.reply({
+                    content:`Something went wrong while executing this command`,
+                    ephermal: true
+                })
+            }
+        }
+    }
+}
---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,62 @@
+const { REST } = require('discord.js')
+const { Routes } = require('discord-api-types/v9')
+
+const fs = require('fs');
+
+module.exports = (client) => {
+    client.handleCommands = async() => {
+        const commandFolders = fs.readdirSync('./src/commands')
+        for (const folder of commandFolders) {
+            const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'))
+            const { commands, commandArray } = client;
+            for (const file of commandFiles) {
+                const command = require(`../../commands/${folder}/${file}`);
+                commands.set(command.data.name, command);
+                commandArray.push(command.data.toJSON());
+                console.log(`Command ${command.data.name} has been passed through the handler`)
+            }
+        }
+
+        const clientId = '1228093850740854824';
+        const guildId = '998642773228015626';
+        // const rest = new REST({ version:'10' }).setToken(process.env.token);
+        // try {
+        //     console.log('Started refreshing applications (/) commands');
+        //     await rest.post(Routes.applicationGuildCommands(clientId, guildId), {
+        //         body: client.commandArray
+        // });
+        //     console.log('Succesfully reloaded applications (/) commands')
+        // } catch (error) {
+        //     console.error(error)
+        // }
+// const rest2 = new REST({ version: '10' }).setToken(process.env.token);
+
+// try {
+//     console.log('Started refreshing applications (/) commands');
+// 	await rest.put(Routes.applicationCommands(clientId), {
+// 		body: {
+// 			content: client.commandArray,
+// 		},
+// 	});
+// } catch (error) {
+// 	console.error(error);
+const rest = new REST().setToken(process.env.token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: client.commandArray },
+		);
+
+		console.log(`Successfully reloaded application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+}}
