b0181656723fefe38030d22e64c8bb3e0ca893cb
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2024-05-01 19:08:40 +0000

    rarities!!!

--- src/commands/tools/embed.js
+++ src/commands/tools/embed.js
@@ -11,19 +11,26 @@ users = usersj.users
 
 const quotes = require('./quotes.json').quotes
 const rarities = require("./rarities.json").rarities
+const q_rarities = require('./quotes_rarities.json').rarities
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('quote-daily')
         .setDescription('get your daily quote'),
         async execute(interaction, client){
-            num = Math.floor(Math.random() * (quotes.length + 1))
-            console.log(num.toString())
+           
+            
+            
+            rarity_roll = rollRarity()
+            rarity = rarities[rarity_roll]
+
+            num = Math.floor(Math.random() * (q_rarities[rarity_roll].length))
             chosen_quote = quotes[0][num]
+            
             quote_embed = new EmbedBuilder()
             .setTitle('You got ' + chosen_quote.name)
-            .setDescription(`The next time you can quote is in <t:${cooldown}:R>`)
-            .setColor(0x18e1ee)
+            .setDescription(`The next time you can quote is <t:${cooldown}:R>`)
+            .setColor(rarity.color)
             .setImage(chosen_quote.quote)
             .setThumbnail(chosen_quote.pfp)
             .setTimestamp(Date.now())
@@ -33,8 +40,8 @@ module.exports = {
             })
             .setURL(chosen_quote.msg_link)
             .addFields([{
-                name: rarities[chosen_quote.rarity.toString()].name,
-                value: chosen_quote.bucks.toString() + ' quotebucks',
+                name: rarity.name,
+                value: rarity.bucks.toString() + ' quotebucks',
                 inline: false
             },
             {
@@ -47,4 +54,29 @@ module.exports = {
                 embeds: [quote_embed]
             })
         }
+}
+
+const rarity_mult = [ // rarity_id : weight
+    {0: 0.649}, // Common
+    {1: 0.25}, // Uncommon
+    {2: 0.05}, // Rare
+    {3: 0.035}, // Epic
+    {4: 0.01}, // Legendary
+    {5: 0.005}, // Unique
+    {6: 0.00001} //The Quote
+]
+
+function rollRarity(){
+    
+    let rand = Math.random()
+    for (let i = 0; i < rarity_mult.length; i++) {
+        const rarity = rarity_mult[i][i]
+        if(rand <= rarity){
+            return i;
+        }
+        rand -= rarity
+        
+    }
+
+    return 0;
 }
