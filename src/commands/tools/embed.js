dc626e739a9bf0e3f89ab475aa8f02a7c4943eb3
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2024-05-05 19:12:10 +0000

    semi broken but there are wroking up

--- src/commands/tools/embed.js
+++ src/commands/tools/embed.js
@@ -7,23 +7,34 @@ asliced = a.toString().slice(0, -3)
 cooldown = parseInt(asliced)
 
 const {createQuoteEmbed} = require('../../util/quoteEmbed')
+const { log } = require('../../util/logger')
 const quotes = require('../../json/quotes.json').quotes
 const rarities = require("../../json/rarities.json").rarities
 const q_rarities = require('../../json/quotes_rarities.json').rarities
 const pfps = require('../../json/pfps.json').pfps
 const rarity_mult = require('../../json/globals.json').rarity_weight
+const mult = require('../../json/luck_mult_temp.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('quote-daily')
         .setDescription('get your daily quote'),
         async execute(interaction, client){
+
             rarity_roll = rollRarity()
             rarity = rarities[rarity_roll]
             
             num = Math.floor(Math.random() * (q_rarities[rarity_roll].length))
-            quote_embed = createQuoteEmbed(q_rarities[rarity_roll][num], cooldown, interaction)
-
+            quote_embed = createQuoteEmbed(q_rarities[rarity_roll][num], cooldown, interaction, mult.money)
+            
+            r = {
+                "luck": 1,
+                "money": 1
+            }
+            var json = JSON.stringify(r)
+        
+            fs.writeFile('./src/json/luck_mult_temp.json', json, (err) => {if(err){throw err}})
+            
             await interaction.reply({
                 embeds: [quote_embed]
             })
@@ -32,9 +43,11 @@ module.exports = {
 
 
 
+
 function rollRarity(){
     
-    let rand = Math.random()
+    let rand = Math.random() * mult.luck
+    log(rand)
     for (let i = 0; i < rarity_mult.length; i++) {
         const rarity = rarity_mult[i][i]
         if(rand <= rarity){
@@ -46,3 +59,4 @@ function rollRarity(){
 
     return 0;
 }
+
---
+++ src/commands/tools/give-upgrade.js
@@ -0,0 +1,78 @@
+const { SlashCommandBuilder, EmbedBuilder, Embed, quote, UserSelectMenuBuilder, ButtonBuilder, ButtonStyle, ActionRow, ActionRowBuilder } = require('discord.js')
+const { execute, name } = require('../../events/client/ready')
+const { createUpgradeEmbed } = require('../../util/quoteEmbed')
+
+const mult = require('../../json/luck_mult_temp.json')
+
+const fs = require('fs');
+const upgrades = require('../../json/upgrades.json')[0]
+const admins = require('../../json/globals.json').admins
+
+const count = Object.keys(upgrades).length
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('give-upgrade')
+        .setDescription('free upgrades!!! (real)')
+        .addIntegerOption(option => option.setName('upgrade-id').setDescription("ID of the upgrade").setRequired(true)),
+        async execute(interaction, client){
+            id = interaction.options.getInteger('upgrade-id').toString()
+            is_admin = false            
+
+            for(let i = 0; i < admins.length; i++){
+                if(admins[i] == interaction.user.id){
+                    is_admin = true
+                    break
+                }
+                
+            }
+
+            if(!is_admin){
+                await interaction.reply({content:"You don't have permission to use this command.", ephemeral: true})
+                return
+
+            }
+
+            if(id < 0 || id > count-1){
+                await interaction.reply({content:`Please choose a number between 0 and ${count-1}.`, ephemeral: true})
+                return
+            }
+
+       
+            const use_btn = new ButtonBuilder()
+            .setCustomId('use')
+            .setLabel('Use')
+            .setStyle(ButtonStyle.Success)
+            
+            const row = new ActionRowBuilder()
+            .addComponents(use_btn)
+
+            const user_filter = i => i.user.id === interaction.user.id
+            
+            upgrade_embed = createUpgradeEmbed(id)
+
+            response = await interaction.reply({
+                embeds: [upgrade_embed],
+                components: [row]
+            })
+
+            const confirm = await response.awaitMessageComponent({filter: user_filter, time: 60_000})
+            if(confirm.customId = 'use'){
+               
+                await interaction.followUp({content: `Gained ${upgrades[id].luck_mult+1}x Luck and ${upgrades[id].money_mult+1}x Money!`, ephemeral: true})
+                await interaction.deleteReply()
+
+                let luck = mult.luck + upgrades[id].luck_mult
+                let money = mult.money + upgrades[id].money_mult
+                r = {
+                    "luck": luck,
+                    "money": money
+                }
+                var json = JSON.stringify(r)
+            
+                fs.writeFile('./src/json/luck_mult_temp.json', json, (err) => {if(err){throw err}})
+
+            }   
+        }
+}
+
