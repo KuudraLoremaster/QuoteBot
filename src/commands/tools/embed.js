6ec26b9650634f0c618b131bf84b4f5cb4f219d9
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-05-07 12:45:21 +0300

    i accidentally deleted pixels shit with the last commit
    whoops

--- src/commands/tools/embed.js
+++ src/commands/tools/embed.js
@@ -1,35 +1,29 @@
-const { SlashCommandBuilder, EmbedBuilder, Embed, quote, UserSelectMenuBuilder, BaseGuildEmoji, roleMention } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, Embed, quote, UserSelectMenuBuilder } = require('discord.js')
 const { execute, name } = require('../../events/client/ready')
-const fs = require('fs');
-const { error } = require('console');
-const sqlite3 = require('sqlite3').verbose();
-const betterSqlite = require('better-sqlite3')
-let sql;
+const fs = require('fs')
 const a = (Date.now() + (24*60*60*1000))
 
 asliced = a.toString().slice(0, -3)
 cooldown = parseInt(asliced)
-const quotes = require('./quotes.json').quotes
-const rarities = require("./rarities.json").rarities
-const q_rarities = require('./quotes_rarities.json').rarities
-const pfps = require('./pfps.json').pfps
+
+const {createQuoteEmbed} = require('../../util/quoteEmbed')
+const { log } = require('../../util/logger')
+const quotes = require('../../json/quotes.json').quotes
+const rarities = require("../../json/rarities.json").rarities
+const q_rarities = require('../../json/quotes_rarities.json').rarities
+const pfps = require('../../json/pfps.json').pfps
+const rarity_mult = require('../../json/globals.json').rarity_weight
 const Database = require('better-sqlite3');
 const db = new Database('users.db', { verbose: console.log });
 query = `CREATE TABLE users(id INTEGER PRIMARY KEY, userID, quotes, quotebucks, tag)` 
 // db.exec(query)
-sql = `INSERT INTO users(userID, quotes, quotebucks) VALUES (?,?,?)`
-// db.run(sql,[317745407599968257, null, null])
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('quote-daily')
-        .setDescription('get your daily quote'),
-        async execute(interaction, client){
-            rarity_roll = rollRarity()
-            rarity = rarities[rarity_roll]
-            num = Math.floor(Math.random() * (q_rarities[rarity_roll].length))
-            chosen_quote = quotes[0][q_rarities[rarity_roll][num]]
+    .setName('quote-daily')
+    .setDescription('get your daily quote'),
+    async execute(interaction, client){
+            const chosen_quote = quotes[0][id]
             sql = `SELECT * FROM users WHERE tag='${interaction.user.tag}'`
-            d = []
             const userArray = db.prepare(sql).all();
             console.log(userArray)
             if (userArray.length == 0) {
@@ -102,44 +96,20 @@ module.exports = {
                     }
                 }
             }
-            quote_embed = new EmbedBuilder()
-            .setTitle('You got ' + chosen_quote.name)
-            .setDescription(`The next time you can quote is <t:${cooldown}:R>`)
-            .setColor(rarity.color)
-            .setImage(chosen_quote.quote)
-            .setThumbnail(pfps[chosen_quote.origin])
-            .setTimestamp(Date.now())
-            .setAuthor({
-                iconURL: interaction.user.displayAvatarURL(),
-                name: interaction.user.tag
-            })
-            .setURL(chosen_quote.msg_link)
-            .addFields([{
-                name: rarity.name,
-                value: rarity.bucks.toString() + ' quotebucks',
-                inline: false
-            },
-            {
-                name: 'Quote by '+ chosen_quote.origin,
-                value: chosen_quote.reason,
-                inline: false    
-            }]);
-
+            rarity_roll = rollRarity()
+            rarity = rarities[rarity_roll]
+            
+            num = Math.floor(Math.random() * (q_rarities[rarity_roll].length))
+            quote_embed = createQuoteEmbed(q_rarities[rarity_roll][num], cooldown, interaction)
+            
             await interaction.reply({
                 embeds: [quote_embed]
             })
         }
 }
 
-const rarity_mult = [ // rarity_id : weight (please make sure it adds up to 1 if you change any of the numbers)
-    {0: 0.649}, // Common
-    {1: 0.25}, // Uncommon
-    {2: 0.05}, // Rare
-    {3: 0.035}, // Epic
-    {4: 0.01}, // Legendary
-    {5: 0.005}, // Unique
-    {6: 0.00001} //The Quote
-]
+
+
 
 function rollRarity(){
     
@@ -155,3 +125,4 @@ function rollRarity(){
 
     return 0;
 }
+
